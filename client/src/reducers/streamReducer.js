535d9f01eef3d412f8b76f1b3cc4698d86e41673
Aurlin Dhillon <aurlindhillon@gmail.com>
2020-02-16 01:18:05 -0800

    comments reducers

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -7,16 +7,23 @@ import {
   DELETE_STREAM
 } from "../actions/types";
 
+// need to combine all the action creators
 export default (state = {}, action) => {
   switch (action.type) {
+    // creating new object, adding in all current objects into object
+    // mapKeys takes the keys of the objects and it a key value
     case FETCH_STREAMS:
       return { ...state, ..._.mapKeys(action.payload, "id") };
+    // redux requirement of always returning a new array hence ...state
+    // dynamically add a new key value
+    // fetch, create, edit all deal with a single record in the redux store
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case CREATE_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload };
+    // payload is the id in delete
     case DELETE_STREAM:
       return _.omit(state, action.payload);
     default:
